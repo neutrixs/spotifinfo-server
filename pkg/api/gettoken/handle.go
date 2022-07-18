b49dfc1e96aca60544cd4b3f43834df6f692ef14
Ihsan Rakhman <ihsan.maulana0501@gmail.com>
2022-07-18 21:47:22 +0700

    check request method and add getCookie func

--- pkg/api/gettoken/handle.go
+++ pkg/api/gettoken/handle.go
@@ -5,5 +5,20 @@ import (
 )
 
 func Handle(w http.ResponseWriter, r *http.Request) {
-	
+	if r.Method != http.MethodPost {
+		errorCode := http.StatusMethodNotAllowed
+		w.WriteHeader(errorCode)
+		w.Write([]byte(http.StatusText(errorCode)))
+		return
+	}
+}
+
+func getCookie(cookies []*http.Cookie, name string) string {
+	for _, c := range cookies {
+		if c.Name == name {
+			return c.Value
+		}
+	}
+
+	return ""
 }
