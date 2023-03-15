9ed9e297a87e4be461ba5f8e4aa7720e8d543f73
Ihsan Rakhman <ihsan.maulana0501@gmail.com>
2023-03-15 18:36:24 +0700

    add SQL DB

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/neutrixs/spotifinfo-server
 go 1.18
 
 require (
+	github.com/go-sql-driver/mysql v1.7.0
 	github.com/gorilla/mux v1.8.0
 	golang.org/x/exp v0.0.0-20220706164943-b4a6d9510983
 )
--- pkg/api/handle.go
+++ pkg/api/handle.go
@@ -1,13 +1,19 @@
 package api
 
 import (
+	"database/sql"
+	"log"
 	"net/http"
 
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
 	"github.com/neutrixs/spotifinfo-server/pkg/api/callback"
 	"github.com/neutrixs/spotifinfo-server/pkg/api/gettoken"
 	"github.com/neutrixs/spotifinfo-server/pkg/api/login"
+	"github.com/neutrixs/spotifinfo-server/pkg/env"
 )
+
+var db *sql.DB
 
 func Handle(w http.ResponseWriter, r *http.Request) {
 	endpoint := mux.Vars(r)["endpoint"]
@@ -27,4 +33,16 @@ func Handle(w http.ResponseWriter, r *http.Request) {
 	status := http.StatusNotFound
 	w.WriteHeader(status)
 	w.Write([]byte(http.StatusText(status)))
+}
+
+func init() {
+	SQL_LOGIN, err := env.Get("SQL_LOGIN")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	db, err = sql.Open("mysql", SQL_LOGIN)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
